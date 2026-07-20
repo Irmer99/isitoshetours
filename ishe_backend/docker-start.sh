57ed592b3dcde483b4df10007803a59b7a0d810f
Patricia <pirmerpatricia99@gmail.com>
2026-07-20 11:07:31 +0300

    fix: remove migration from docker-start, add GH Action for prisma deploy

--- ishe_backend/docker-start.sh
+++ ishe_backend/docker-start.sh
@@ -1,8 +1,5 @@
 #!/bin/sh
 set -e
 
-echo "Running migrations via direct connection..."
-DATABASE_URL="$DIRECT_DATABASE_URL" npx prisma migrate deploy
-
 echo "Starting server..."
 exec node index.js
